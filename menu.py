382b22d7724e3ff12c60e94fdb02daf03f89adad
Lucas Rocha <lucasrochalusantos@gmail.com>
2026-08-14 17:58:55 -0300

    menu.py
    Implementa opção de somar

---
+++ menu.py
@@ -0,0 +1,14 @@
+def menu():
+    print("1. Somar")
+    print("2. Subtrair")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("0. Sair")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        numero1 = float(input("Digite o primeiro número: "))
+        numero2 = float(input("Digite o segundo número: "))
+        resultado = numero1 + numero2
+        print("Resultado:", resultado)
